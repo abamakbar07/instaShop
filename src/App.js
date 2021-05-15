a75423e235fceeb348387719956573d50c609527
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-15 20:47:54 +0700

    add Admin page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Dashboard from './pages/dashboard/Dashboard';
 import Navigationbar from './pages/components/navigationbar/Navigationbar'
 import Product from './pages/product/Product';
+import Admin from './pages/admin/Admin';
 
 function App() {
   return (
     <div className="App">
       <Navigationbar />
-      <Product />
+      <Admin />
     </div>
   );
 }
---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const Admin = () => {
+   return (
+      <div className="Admin">
+         ADMIN PAGE
+         <Button variant="danger">
+            reauthorize
+         </Button>
+      </div>
+   )
+}
+
+export default Admin
