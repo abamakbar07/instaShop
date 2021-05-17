e0811c5925c14ec2557e4b2daeefeac5088e4266
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-17 10:27:24 +0700

    restyle Fetching data produk from instagram account

--- src/pages/components/productcard/ProductCard.jsx
+++ src/pages/components/productcard/ProductCard.jsx
@@ -3,10 +3,15 @@ import { Button, Card } from 'react-bootstrap'
 import './productcard.css'
 
 const ProductCard = (props) => {
+   props.media_type == "VIDIO" ? console.log("VIDIO") : console.log("NOT_VIDIO")
    return (
      <div className="ProductCard">
        <Card style={{ width: "18rem" }}>
-         <Card.Img variant="top" src="holder.js/100px180" />
+         {/* {props.media_type == "VIDIO" ? (
+           <iframe src={props.img} />
+         ) : ( */}
+           <Card.Img variant="top" src={props.img} />
+         {/* )} */}
          <Card.Body>
            <Card.Title className="title">{props.title}</Card.Title>
            <Card.Text className="desc line-clamp">{props.desc}</Card.Text>
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,50 +1,59 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { CardColumns, Col, Container, Row, Spinner } from 'react-bootstrap'
 import { instagramAPI } from '../../config/api'
 import ProductCard from '../components/productcard/ProductCard'
 
 const Product = () => {
-   const [data, setData] = useState(null)
-   const getData = async () => {
-      try {
-         const param = {
-            params : {
-               fields: "id,caption",
-               access_token:
-                 "IGQVJWYzBqX2M1YU9KTnpjZAm9nbDRMLVVJTVhHNnQ2TEkzeTdxUFJHa2ItODNTWlNnMW5tdUs4WUxJZAjl6N2YxYXFQSVVYbDlJVFNhXzFPYThkdTBLWG9tenNacjRpVk52SmVnbXJkRTgyT2MxQ3FfNQZDZD",
-               }
-            };
-         const result = await instagramAPI.get('/me/media', param)
-         console.log(result)
-      } catch (error) {
-         console.log(data)
-      }
-   }
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
+  const getData = async () => {
+    try {
+        const param = {
+          params: {
+            fields: "id,media_type,caption,media_url,thumbnail_url",
+            access_token:
+              "IGQVJWYzBqX2M1YU9KTnpjZAm9nbDRMLVVJTVhHNnQ2TEkzeTdxUFJHa2ItODNTWlNnMW5tdUs4WUxJZAjl6N2YxYXFQSVVYbDlJVFNhXzFPYThkdTBLWG9tenNacjRpVk52SmVnbXJkRTgyT2MxQ3FfNQZDZD",
+          },
+        };
+        const result = await instagramAPI.get('/me/media', param)
+        setData(result.data.data)
+        setLoading(false)
+    } catch (error) {
+        console.log(error)
+    }
+  }
 
-   useEffect(() => {
-      getData()
-   }, [])
-   
-   return (
-     <Container className="Product bg-danger">
-       <Row>
-         <Col md={2} className="bg-light">
-           Filter
-         </Col>
-         <Col md={10} className="bg-dark">
-           <Row>
-             <Col md={4}>
-               <ProductCard
-                 title="Product pertama dengan text panjang"
-                 desc="Some quick example text to build on the card title and make up the
-             bulk of the card's content. dengan text yang panjang dan melewati batas"
-               />
-             </Col>
-           </Row>
-         </Col>
-       </Row>
-     </Container>
-   );
+  useEffect(() => {
+    getData()
+  }, [])
+  
+  return (
+    <Container className="Product bg-danger">
+      <Row>
+        <Col md={2} className="bg-light">
+          Filter
+        </Col>
+        <Col md={10} className="bg-dark">
+          <CardColumns>
+          {loading ? (
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
+          ) : (
+            data.map((post) => (
+              <ProductCard
+                media_type={post.media_type}
+                img={post.media_type == "VIDIO" ? post.thumbnail_url : post.media_url}
+                title={post.id}
+                desc={post.caption}
+                />
+              ))
+            )}
+          </CardColumns>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Product
