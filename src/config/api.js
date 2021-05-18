e5e711fb0797359a34b85ac6e7fc42d4ea5f276f
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-19 00:19:30 +0700

    add server API

--- src/config/api.js
+++ src/config/api.js
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const instagramAPI = axios.create({
   baseURL: "https://graph.instagram.com",
 });
+
+export const API = axios.create({
+  baseURL: "http://localhost:3000/",
+});
