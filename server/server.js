2663f9731b59e6d540b9167d1f9f623ea034649e
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-18 17:11:13 +0700

    configure heroku deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 
 require('dotenv').config()
 
@@ -12,6 +13,11 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(bodyParser.json())
 app.use('/api/v1/', router)
+
+app.use(express.static(path.join(__dirname, "../build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build"));
+});
 
 app.listen(port, () => {
    console.log(`Server running on port: ${port}`)
