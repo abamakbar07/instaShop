6b2f856f336b3c71ba47bcd9afe30a969f61b62b
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-22 01:05:16 +0700

    restyle dashboard

--- src/pages/components/Button.jsx
+++ src/pages/components/Button.jsx
@@ -8,3 +8,16 @@ export const ButtonPrimary = (props) => (
 export const ButtonSecondary = (props) => (
    <div className={`secondary btn title ${props.className}`}>{props.text}</div>
 )
+
+export const ButtonArrowPrimary = (props) => (
+  <div className={`arrow primary btn title ${props.className}`}>
+    <span>{props.text}</span>
+  </div>
+);
+
+export const ButtonArrowSecondary = (props) => (
+  <div className={`arrow secondary btn title ${props.className}`}>
+    <span>{props.text}</span>
+  </div>
+);
+
--- src/pages/dashboard/views/HeaderDashboard.jsx
+++ src/pages/dashboard/views/HeaderDashboard.jsx
@@ -1,34 +1,26 @@
 import React, { useState } from 'react'
-import { Carousel, Col, Container } from 'react-bootstrap'
-import { ButtonPrimary, ButtonSecondary } from '../../components/Button';
+import { Col, Container } from 'react-bootstrap'
+import { ButtonArrowPrimary, ButtonArrowSecondary } from '../../components/Button';
 import '../dashboard.css'
 
 const HeaderDashboard = () => {
-  const [index, setIndex] = useState(0);
-  
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
-
   return (
     <div className="HeaderDashboard">
       <Container>
-        <Col sm={5} className="">
+        <Col sm={6} className="">
           <div className="header title">
             Beautiful For The Everyday Activity
           </div>
           <div className="header desc">
             be pretty and make them fall in love with you
           </div>
-          <div className="row p-0 m-0 mr-5 justify-content-between">
-            <ButtonPrimary text="Order Now" />
-            <ButtonSecondary text="Learn About Us" />
-            {/* <div className="btn btn-danger mr-auto">
-              Order Now
-            </div>
-            <div className="btn btn-danger">
-              Learn About Us
-            </div> */}
+          <div className="row p-0 m-0 mr-5">
+            <Col sm={6} className="p-0 pt-2">
+              <ButtonArrowPrimary text="Order Now" />
+            </Col>
+            <Col sm={6} className="p-0 pt-2">
+              <ButtonArrowSecondary text="Learn About Us" />
+            </Col>
           </div>
         </Col>
       </Container>
