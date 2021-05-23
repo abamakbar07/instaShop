b12479d16ffa3d1f8b04e9845a3155931463f52a
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-23 22:55:48 +0700

    styling best product

--- src/pages/dashboard/views/BestProductsDashboard.jsx
+++ src/pages/dashboard/views/BestProductsDashboard.jsx
@@ -6,35 +6,37 @@ import best2 from '../../../assets/7.png'
 
 
 const BestProductsDashboard = () => {
-   return (
-     <div className="BestProductsDashboard">
-       <Col>
-         <div className="text-center">Best Products</div>
-         <Container>
-           <CardColumns>
-             <BestProductsCard
-               title="Produk Pertama"
-               desc="Some quick example text to build on the card title and make up the
-                     bulk of the card's content."
-               img={best1}
-             />
-             <BestProductsCard
-               title="Second Product"
-               desc="Some quick example text to build on the card title and make up the
-                     bulk of the card's content."
-               img={best2}
-             />
-             <BestProductsCard
-               title="Atsaltun prod"
-               desc="Some quick example text to build on the card title and make up the
-                     bulk of the card's content."
-               img={best1}
-             />
-           </CardColumns>
-         </Container>
-       </Col>
-     </div>
-   );
+  return (
+    <div className="BestProductsDashboard">
+      <div className="Border">
+        <Col>
+          <div className="title text-center p-3">Best Products</div>
+          <Container>
+            <CardColumns>
+              <BestProductsCard
+                title="Produk Pertama"
+                desc="Some quick example text to build on the card title and make up the
+                    bulk of the card's content."
+                img={best1}
+              />
+              <BestProductsCard
+                title="Second Product"
+                desc="Some quick example text to build on the card title and make up the
+                    bulk of the card's content."
+                img={best2}
+              />
+              <BestProductsCard
+                title="Atsaltun prod"
+                desc="Some quick example text to build on the card title and make up the
+                    bulk of the card's content."
+                img={best1}
+              />
+            </CardColumns>
+          </Container>
+        </Col>
+      </div>
+    </div>
+  );
 }
 
 export default BestProductsDashboard
