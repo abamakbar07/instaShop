82a7b279eda3d96dc92c3f6a2ba25b7248208ab7
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-23 23:25:21 +0700

    props onClick button

--- src/pages/components/Button.jsx
+++ src/pages/components/Button.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import './style.css'
 
 export const ButtonPrimary = (props) => (
-   <div className={`primary btn title ${props.className}`}>{props.text}</div>
+   <div className={`primary btn title ${props.className}`} onClick={props.onClick}>{props.text}</div>
 )
 
 export const ButtonSecondary = (props) => (
-   <div className={`secondary btn title ${props.className}`}>{props.text}</div>
+   <div className={`secondary btn title ${props.className}`} onClick={props.onClick}>{props.text}</div>
 )
 
 export const ButtonArrowPrimary = (props) => (
-  <div className={`arrow primary btn title ${props.className}`}>
+  <div className={`arrow primary btn title ${props.className}`} onClick={props.onClick}>
     <span>{props.text}</span>
   </div>
 );
 
 export const ButtonArrowSecondary = (props) => (
-  <div className={`arrow secondary btn title ${props.className}`}>
+  <div className={`arrow secondary btn title ${props.className}`} onClick={props.onClick}>
     <span>{props.text}</span>
   </div>
 );
