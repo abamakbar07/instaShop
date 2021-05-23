1944d586abd991dd82dedcd3404e2378bcb49221
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-05-23 23:10:35 +0700

    restyle best product card

--- src/pages/components/bestproductscard/BestProductsCard.jsx
+++ src/pages/components/bestproductscard/BestProductsCard.jsx
@@ -4,21 +4,20 @@ import './bestproductscard.css'
 
 const BestProductsCard = (props) => {
    return (
-      <div className="BestProductsCard">
-         <Card>
-            <Card.Img variant="top" src={props.img} />
-            <Card.ImgOverlay>
-               <Card.Body>
-                  <Card.Title>
-                     {props.title}
-                  </Card.Title>
-                  <Card.Text>
-                     {props.desc}
-                  </Card.Text>
-               </Card.Body>
-            </Card.ImgOverlay>
-         </Card>
-      </div>
+     <div className="BestProductsCard">
+       <Card>
+         <Card.Img variant="top" src={props.img} />
+         <Card.ImgOverlay>
+           <Card.Body>
+             <Card.Title>{props.title}</Card.Title>
+             <Card.Text>{props.desc}</Card.Text>
+           </Card.Body>
+         </Card.ImgOverlay>
+         <Card.Body>
+           <Card.Title className="title">{props.title}</Card.Title>
+         </Card.Body>
+       </Card>
+     </div>
    );
 }
 
