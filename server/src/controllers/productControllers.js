29edcd9da8aae6a9c4e02134cd0a83029b99eaa7
Akbar Afriansyah <abam.akbar07@gmail.com>
2021-06-13 16:39:48 +0700

    add getProducts route from Instagram using Axios

--- server/src/controllers/productControllers.js
+++ server/src/controllers/productControllers.js
@@ -1,9 +1,41 @@
+const axios = require('axios')
 const Product = require('../../models/productModel')
 
 exports.getProducts = async (req, res) => {
-   Product.find()
-      .then(products => res.json(products))
-      .catch(err => console.log(err))
+      const instagramAPI = axios.create({
+            baseURL: "https://graph.instagram.com",
+      });
+      try {
+            const param = {
+            params: {
+                  fields: "id,media_type,caption,media_url,thumbnail_url",
+                  access_token:
+                  "IGQVJWYzBqX2M1YU9KTnpjZAm9nbDRMLVVJTVhHNnQ2TEkzeTdxUFJHa2ItODNTWlNnMW5tdUs4WUxJZAjl6N2YxYXFQSVVYbDlJVFNhXzFPYThkdTBLWG9tenNacjRpVk52SmVnbXJkRTgyT2MxQ3FfNQZDZD",
+            },
+            };
+            const result = await instagramAPI.get('/me/media', param)
+            const data = result.data.data
+
+            res.json({
+                  status: "Success",
+                  message: "Product successfully loaded",
+                  data,
+
+            })            
+            // dispatchProduct({
+            //    type: "GET_PRODUCTS",
+            //    payload: {
+            //       data,
+            //    }
+            // })
+            
+      } catch (error) {
+            console.log(error)
+      }
+   
+//    Product.find()
+//       .then(products => res.json(products))
+//       .catch(err => console.log(err))
 }
 
 exports.addProduct = async (req, res) => {
